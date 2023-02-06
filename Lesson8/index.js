1e5c533c89242574bf0ec6f92fa643fb5bd38ef4
Nightowlam <nightowl3.00am@gmail.com>
2023-02-06 10:11:12 +0600

    route and header parameter

--- Lesson8/index.js
+++ Lesson8/index.js
@@ -2,10 +2,24 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/user", (req, res) => {
+// Query parameter:
+app.get("/user1", (req, res) => {
   const { id, name, age } = req.query;
-
   res.send(`User id: ${id}, user name: ${name} and age:${age}`);
+});
+
+// Route parameter:
+app.get("/userId/:id/userAge/:age", (req, res) => {
+  const { id, age } = req.params;
+  res.send(`User id: ${id}, Age:${age}`);
+});
+
+// Header parameter:
+app.get("/user3", (req, res) => {
+  const id = req.header("id");
+  const name = req.header("name");
+  const age = req.header("age");
+  res.send(`User id: ${id}, Name:${name}, Age: ${age}`);
 });
 
 app.use("/", (req, res) => {
