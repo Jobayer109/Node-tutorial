50c1a5f93b864c1c9ce5488bfc0b8a6223158c88
Nightowlam <nightowl3.00am@gmail.com>
2023-02-06 09:17:28 +0600

    http response and coookie implemented

--- Lesson7/app.js
+++ Lesson7/app.js
@@ -4,8 +4,20 @@ const userRouter = require("./routes/user.routes");
 
 app.use("/api/user", userRouter);
 
+app.use("/register", (req, res) => {
+  res.statusCode = 200;
+  res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/login", (req, res) => {
+  res.cookie("name", "Jobayer");
+  res.cookie("age", "30");
+  res.end();
+});
+
 app.use("/", (req, res) => {
-  res.send("This is Home page");
+  res.statusCode = 200;
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.use((req, res) => {
